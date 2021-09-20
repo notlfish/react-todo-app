69e0052ad5ab1957fecfb75e3cac1ec9e8e44fd9
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-09-20 15:28:41 -0300

    Add checkbox to TodoItem components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class TodoItem extends React.Component {
   render() {
-    const { todo: { title } } = this.props;
-    return <li>{title}</li>;
+    const { todo: { title, completed } } = this.props;
+    return (
+      <li>
+        <input type="checkbox" checked={completed} />
+        {title}
+      </li>
+    );
   }
 }
 
 TodoItem.propTypes = {
   todo: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
   }).isRequired,
 };
 
