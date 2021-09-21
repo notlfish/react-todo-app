4f0648e4da633647b7fa297d77a7067867837560
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-09-21 18:27:51 -0300

    Fix linting errors

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaTrash } from 'react-icons/fa';
 import styles from './TodoItem.module.scss';
-import { FaTrash } from "react-icons/fa"
 
 class TodoItem extends React.Component {
   constructor(props) {
